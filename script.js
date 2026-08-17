5b037762b8cb8d84f6437f92e7aecff9e65788d3
hitesh <bhatnagerhitesh@gmail.com>
2026-08-17 19:39:28 +0530

    allmost complete

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 const searchBox = document.querySelector(".search-input")
-const movieList = document.querySelector(".movie-grid")
+const grid = document.querySelector(".movie-grid")
 API_KEY = "38206dc3"
 
 let wait;
@@ -12,6 +11,7 @@ searchBox.addEventListener('input', (e) => {
     wait = setTimeout(() => {
         if (value) {
             api(value)
+            grid.innerHTML = ""
         }
     }, 900)
 })
@@ -19,30 +19,43 @@ searchBox.addEventListener('input', (e) => {
 function api(movie) {
     fetch(`https://omdbapi.com/?s=${movie}&apikey=${API_KEY}`)
         .then((res) => res.json())
-        .then((res) => { render(res.Search) })
+        .then((res) => { render(res, movie) })
 }
 
-function render(data) {
-    movieList.innerHtml = data.map((movie) => {
-        `<a href="movie.html" target="_blank" rel="noopener noreferrer">
-                    <div class="movie-card">
-                        <img src="${movie.Poster}"
-                            alt="${movie.Title}">
+function render(data, value) {
+    const movieList = data.Search?.map((movie) => (
+        `<div class="movie ${movie.Title}" >
+        <a href="movie.html" target="_blank" rel="noopener noreferrer">
+                <div class="movie-card">
+                    <img src="${movie.Poster}"
+                        alt="${movie.Title}">
 
-                        <div class="movie-info">
-                            <h3 class="movie-title">${movie.Title}</h3>
+                    <div class="movie-info">
+                        <h3 class="movie-title">${movie.Title}</h3>
 
-                            <div class="movie-meta">
-                                <span>${movie.Year}</span>
-                                <span>⭐ ${movie.imdbID}</span>
-                            </div>
-
-                            <p class="movie-genre">
-                                ${movie.Type}
-                            </p>
+                        <div class="movie-meta">
+                            <span>${movie.Year}</span>
+                            <span>⭐ ${movie.imdbID}</span>
                         </div>
-                    </div>
 
-                </a>`.join("")
-    })
+                        <p class="movie-genre">
+                            ${movie.Type}
+                        </p>
+                    </div>
+                </div>
+
+            </a>
+            </div>`
+    )).join("")
+    if (movieList) {
+        grid.innerHTML = movieList
+    } else {
+        grid.innerHTML = `<div class="not-found">${value} not exist</div>`
+    }
+
+    searchBox.value = ""
 }
+
+grid.addEventListener("click", (e) => {
+    console.log(e.target.closest('div'))
+})
