dbd4f6ed3fed5450dea80e95851cc100a80e93fe
hitesh <bhatnagerhitesh@gmail.com>
2026-08-17 18:50:47 +0530

    fixing apit key

--- script.js
+++ script.js
@@ -19,26 +19,26 @@ searchBox.addEventListener('input', (e) => {
 function api(movie) {
     fetch(`https://omdbapi.com/?s=${movie}&apikey=${API_KEY}`)
         .then((res) => res.json())
-        .then((res) => { console.log(res.Search) })
+        .then((res) => { render(res.Search) })
 }
 
-function render() {
-    movieList.innerHtml = [].map((movie) => {
+function render(data) {
+    movieList.innerHtml = data.map((movie) => {
         `<a href="movie.html" target="_blank" rel="noopener noreferrer">
                     <div class="movie-card">
-                        <img src="${movie.poster}"
-                            alt="${movie.name}">
+                        <img src="${movie.Poster}"
+                            alt="${movie.Title}">
 
                         <div class="movie-info">
-                            <h3 class="movie-title">${movie.name}</h3>
+                            <h3 class="movie-title">${movie.Title}</h3>
 
                             <div class="movie-meta">
-                                <span>${movie.year}</span>
-                                <span>⭐ ${movie.imbd}</span>
+                                <span>${movie.Year}</span>
+                                <span>⭐ ${movie.imdbID}</span>
                             </div>
 
                             <p class="movie-genre">
-                                ${movie.genre}
+                                ${movie.Type}
                             </p>
                         </div>
                     </div>
